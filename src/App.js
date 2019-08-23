a365512d3053af446bbe9b456b179e9ce8cef208
Akshay B Nair <akshaybnair226@gmail.com>
2019-08-23 14:10:17 +0530

    added navigation to menu-items

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import './App.css'
 
+const HatsPage = () => (
+  <h1>Hats page</h1>
+)
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={HomePage}></Route>
+          <Route path='/hats' component={HatsPage}></Route>
+        </Switch>
+      </div>
+    
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,7 +49,7 @@ class Directory extends React.Component {
         <div className='directory-menu'>
             {
                 this.state.sections.map(
-                    ({title,imageUrl,id, size}) => <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ({id,...otherSectionprops}) => <MenuItem key={id} {...otherSectionprops}/>
                 )
             }
         </div>
