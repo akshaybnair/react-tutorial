bef77aa3d06b5bad4b215fadbe7104d8fd5a5d55
Akshay B Nair <akshaybnair226@gmail.com>
2019-08-27 14:47:52 +0530

    added cart icon, cart dropdown components. Also implemented cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,9 +5,12 @@ import {auth} from '../../firebase/firebase.util';
 import {connect} from 'react-redux';
 
 import {ReactComponent as Logo} from  '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className="header">
         <Link to="/" className="logo-container">
             <Logo className="logo">
@@ -23,11 +26,17 @@ const Header = ({currentUser}) => (
                 :
                 <Link className="options" to="/signin">Sign in</Link>
             }
+            <CartIcon/>
         </div>
+        {
+            hidden ? null : (<CartDropdown/>) 
+        }
+        
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser : state.user.currentUser
+const mapStateToProps = ({user : {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 export default connect(mapStateToProps)(Header);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,14 +1,12 @@
+import {UserActionTypes} from './user.types';
+
 const INITIAL_STATE = {
     currentUser : null
 };
 const userReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
-        case 'SET_CURRENT_USER' :
-            console.log('-----------------action-----------------')
-            console.log(action)
-            console.log('-----------------payload-----------------')
-            console.log(action.payload)           
-            return {
+        case UserActionTypes.SET_CURRENT_USER :
+                return {
                 ...state,
                 currentUser: action.payload
             };
