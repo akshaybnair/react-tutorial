3605d49f6487279cb6b5d5b0a16fbf5bdf4bec16
Akshay B Nair <akshaybnair226@gmail.com>
2019-08-30 20:06:02 +0530

    added reselect from cartitems in dropdown and cartitemscount

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemCount} from '../../redux/cart/cart.selectors';
 
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className="cart-icon" onClick={toggleCartHidden} >
         <ShoppingIcon className="shopping-icon"></ShoppingIcon>
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden : () => dispatch(toggleCartHidden())
-})
+});
+
+const mapStateToProps = ( state ) => ({
+  itemCount: selectCartItemCount(state)
+});
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
