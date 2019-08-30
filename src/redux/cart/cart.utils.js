d378e645a14c8fbab7c80db72ce143c53c5e1ce5
Akshay B Nair <akshaybnair226@gmail.com>
2019-08-30 22:26:39 +0530

    added additem and removeitem functionality to checkoutitem component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,9 +1,7 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartitem = cartItems.find(
-    cartItem => {
-      return cartItem.id === cartItemToAdd.id
-    }
-  );
+  const existingCartitem = cartItems.find(cartItem => {
+    return cartItem.id === cartItemToAdd.id;
+  });
   if (existingCartitem) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
@@ -11,5 +9,23 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         : cartItem
     );
   }
-  return [...cartItems, { ...cartItemToAdd, quantity: 1 }  ];
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartitem = cartItems.find(cartItem => {
+    return cartItem.id === cartItemToRemove.id;
+  });
+  if (existingCartitem.quantity === 1) {
+    console.log('------------after filter---------')
+    console.log(cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id));
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+  }
+
+  console.log({cartItemToRemove});
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItemToRemove.quantity - 1 }
+      : cartItem
+  );
 };
